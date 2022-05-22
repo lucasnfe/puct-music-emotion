5039010fcb73e017cee69feed68cab286d0a170c
Lucas N. Ferreira <lucasnfe@gmail.com>
2022-05-22 01:38:33 -0700

    Disable discriminator

--- workspace/mcts.py
+++ workspace/mcts.py
@@ -159,12 +159,13 @@ class MCTS:
 
     def _reward(self, state):
         "Returns the reward for a random simulation (to completion) of `node`"
+        #roll_state = state.unsqueeze(0)
         roll_state = self._rollout(state, depth=1)
         print("continuation", roll_state)
         
         # Discriminator score
-        y_hat = self.discriminator(roll_state)
-        discriminator_score = torch.sigmoid(y_hat).squeeze()
+        #y_hat = self.discriminator(roll_state)
+        #discriminator_score = torch.sigmoid(y_hat).squeeze()
 
         # Emotion score
         y_hat = torch.softmax(self.emotion_classifier(roll_state), dim=1).squeeze()
@@ -174,12 +175,13 @@ class MCTS:
 
         if emotion_hat == self.emotion:
             #reward = emotion_score * discriminator_score
-            reward = discriminator_score
+            #reward = discriminator_score
+            reward = emotion_score
         else:
             reward = (emotion_score - 1.0)# * (1.0 - discriminator_score)
 
-        print("reward", emotion_hat, discriminator_score, reward)
-        #print("reward", emotion_hat, emotion_score, reward)
+        #print("reward", emotion_hat, discriminator_score, reward)
+        print("reward", emotion_hat, emotion_score, reward)
         return reward
 
     def _select(self, s, eps=1e-8):
